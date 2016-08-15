94df727f30056f8877a8f9cd70e367ebe41ff83e
Michael Murray <mmurray@qumulo.com>
2016-08-15 16:09:23 -0700

    forgot concatenator

--- qsplit.py
+++ qsplit.py
@@ -62,7 +62,7 @@ class Bucket:
             path = current_path + entry['name']
             # path = '"' + path_prefix + path.replace('/','\\') + '"'
             # print "prefix is " + path_prefix + " and path is " + path
-            path = '"' path.replace('/','\\') + '"'
+            path = '"' + path.replace('/','\\') + '"'
         else:
             path = current_path + entry['name']
 
